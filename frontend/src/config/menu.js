4dcc653d2c3ba70a339b418740a11954420b53a1
chayanonr <chayanonrodw@gmail.com>
2024-12-18 17:12:17 +0700

    update menu images

--- frontend/src/config/menu.js
+++ frontend/src/config/menu.js
@@ -1,12 +1,12 @@
 // src/config/menu.js
 
-import redSet from "../assets/images/red-set.jpg";
-import greenSet from "../assets/images/green-set.jpg";
-import blueSet from "../assets/images/blue-set.jpg";
-import yellowSet from "../assets/images/yellow-set.jpg";
-import pinkSet from "../assets/images/pink-set.jpg";
-import purpleSet from "../assets/images/purple-set.jpg";
-import orangeSet from "../assets/images/orange-set.jpg";
+import redSet from "../assets/images/red-set.png";
+import greenSet from "../assets/images/green-set.png";
+import blueSet from "../assets/images/blue-set.png";
+import yellowSet from "../assets/images/yellow-set.png";
+import pinkSet from "../assets/images/pink-set.png";
+import purpleSet from "../assets/images/purple-set.png";
+import orangeSet from "../assets/images/orange-set.png";
 
 export const menuItems = [
   { name: "Orange", price: 120, image: orangeSet },
